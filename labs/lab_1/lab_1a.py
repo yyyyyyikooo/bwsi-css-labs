4ab286c215ec3f8e45f4e5b8daa9876ad146914f
Cathy Lyu <lyuqixuan@gmail.com>
2026-03-03 21:14:44 -0500

    Add introduction in lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Cathy Lyu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! My name is Cathy and I am a Sophomore at Hackley. I am really interested in STEM and I wish to learn more about cybersecurity and gain more hands-on experiences with hardwares and physical engineering. I know how to code languages such as Java, Python, and C++. I am excited to learn from everyone!")
 
 if __name__ == "__main__":
     main()

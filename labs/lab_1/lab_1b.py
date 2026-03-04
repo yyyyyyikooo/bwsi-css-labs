34687d98171417c5e3a9e5dc392d295af71efd7f
Cathy Lyu <lyuqixuan@gmail.com>
2026-03-04 14:48:11 -0500

    Add input sanitation to lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -36,14 +36,29 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             raise ValueError("Cannot divide by zero.")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+    
+def request_sanitize_input(prompt: str) -> float:
+    """
+        Function that sanitizes user input for the operation
+
+        Returns:
+
+    """
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please input a valid number.")
+        
+
 
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_sanitize_input("Enter the first number: ")
+    num2 = request_sanitize_input("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result

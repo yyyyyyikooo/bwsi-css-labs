cde8f7d1cff54b4e5070993101dd586f5b4c460b
Cathy Lyu <lyuqixuan@gmail.com>
2026-03-09 08:32:25 -0400

    Add unit tests for simple_calculator

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -11,21 +11,26 @@ def test_addition():
     assert simple_calculator("add", 5, 3) == 8          # Test for positive numbers
     assert simple_calculator("add", -2, 2) == 0         # Test for negative and positive number
     assert simple_calculator("add", 0, 0) == 0          # Test for zero addition
+    assert simple_calculator("add", -1, -1) == -2       # Test for negative addition
 
 def test_subtraction():
     assert simple_calculator("subtract", 5, 3) == 2     # Test for positive numbers
     assert simple_calculator("subtract", -2, -2) == 0   # Test for negative numbers
     assert simple_calculator("subtract", 0, 5) == -5    # Test for zero minuend
+    assert simple_calculator("subtract", -1, 1) == -2   # Test for negative and positive number
+    assert simple_calculator("subtract", 1, -1) == 2    # Test for negative and positive number
 
 def test_multiplication():
     assert simple_calculator("multiply", 5, 3) == 15    # Test for positive numbers
     assert simple_calculator("multiply", -2, 2) == -4   # Test for negative and positive number
     assert simple_calculator("multiply", 0, 100) == 0   # Test for multiplication by zero
+    assert simple_calculator("multiply", -1, -9) == 9   # Test for negative numbers
 
 def test_division():
     assert simple_calculator("divide", 6, 3) == 2       # Test for positive numbers
     assert simple_calculator("divide", -4, 2) == -2     # Test for negative and positive number
     assert simple_calculator("divide", 5, 2) == 2.5     # Test for division resulting in float
+    assert simple_calculator("divide", -4, -2) == 2     # Test for ngative numbers
 
 def test_division_by_zero():
     with pytest.raises(ValueError, match="Cannot divide by zero."):
